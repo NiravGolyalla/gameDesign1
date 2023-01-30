7f1c274a5f04dffdd8715a9b510e571c8245d5e2
NiravGolyalla <ng2411@nyu.edu>
2023-01-30 18:06:26 -0500

    Fixing Stuff

--- My project/Assets/Player.cs
+++ My project/Assets/Player.cs
@@ -15,7 +15,11 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
+<<<<<<< HEAD
             // this.gameObject.getComponent()<SpriteRender>.color = Color.blue;
+=======
+            gameObject.getComponent()<SpriteRender>.color = Color.blue;
+>>>>>>> parent of 798205d (Added Triangles + Spawners)
         }
     }
 }
